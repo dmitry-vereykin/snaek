7b0b862e4bfffdc0941287244cf1c36b41c76d71
Nader Abdelrahman <abdelrahmannader@gmail.com>
2018-06-19 10:27:23 -0400

    Add Snake Game

---
+++ The Game/javascript/settings.js
@@ -0,0 +1,9 @@
+let canvas_context = canvas.getContext('2d');
+let snake_size = 10;
+let canvas_width = 350;
+let canvas_height = 350;
+let score = 0;
+let snake;
+let food;
+let lootbox;
+let cntr = 0;
