38f490892ac28ecd8365526543a6bf10c36024bc
Aaron <aaronpng@gmail.com>
2018-06-19 13:39:10 -0400

    Delete settings.js

--- The Game/javascript/settings.js
+++
@@ -1,9 +0,0 @@
-let canvas_context = canvas.getContext('2d');
-let snake_size = 10;
-let canvas_width = 350;
-let canvas_height = 350;
-let score = 0;
-let snake;
-let food;
-let lootbox;
-let cntr = 0;
